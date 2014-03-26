1199d365f0d49b602eda8ac7d25b3ee68fdb2e0f
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-03-26 18:58:47 -0400

    skill buttons now turn green when clicked

---
+++ app/assets/javascripts/skills.js
@@ -0,0 +1,12 @@
+var Skills = Skills || {};
+
+$(document).ready(function() {
+  $('.skill-button').click(Skills.changeColor);
+});
+
+Skills.changeColor = function(event) {
+  event.preventDefault();
+  $(this).toggleClass("btn-primary");
+  $(this).toggleClass("btn-success");
+  return false;
+};
