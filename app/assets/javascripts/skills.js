e2d852f4b963e07908708dab69705a6388fa4938
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-03-27 03:48:19 -0400

    clicking and unclicking skills passes params almost correctly

--- app/assets/javascripts/skills.js
+++ app/assets/javascripts/skills.js
@@ -1,17 +1,34 @@
-var Skills = Skills || {},
+var Skills = Skills || { currentSkills: [] },
     Recipes = Recipes || {};
 
 $(document).ready(function() {
-  $('.skill-button').click(Skills.changeColor);
+  $('.skill-button').click(Skills.selectSkill);
   $('#skills-done-button').click(Recipes.getCurrentRecipes);
 });
 
-Skills.changeColor = function(event) {
-  event.preventDefault();
+Skills.selectSkill = function(event) {
+  if (event.target.className.match("btn-primary")) {
+    Skills.addSkill($(this));
+  }
+  if (event.target.className.match("btn-success")) {
+    Skills.removeSkill($(this));
+  }
   $(this).toggleClass("active");
   $(this).toggleClass("btn-primary");
   $(this).toggleClass("btn-success");
-  return false;
+};
+
+Skills.addSkill = function(button) {
+  Skills.currentSkills.push(button.attr('data-skill-id'));
+  console.log("Skills.currentSkills:" + Skills.currentSkills);
+};
+
+Skills.removeSkill = function(button) {
+  var this_skill = Skills.currentSkills.indexOf(button.attr('data-skill-id'));
+  // change this line
+  // remove element from array by index (instead of pop)
+  Skills.currentSkills.pop(this_skill);
+  console.log("Skills.currentSkills:" + Skills.currentSkills);
 };
 
 Recipes.getCurrentRecipes = function(event) {
@@ -20,7 +37,7 @@ Recipes.getCurrentRecipes = function(event) {
     url: '/skills',
     type: 'POST',
     dataType: 'JSON',
-    data: {known_skills: [1,5]}
+    data: {known_skills: Skills.currentSkills}
   })
   .done(function(data) {
     console.log("success");
@@ -34,9 +51,9 @@ Recipes.getCurrentRecipes = function(event) {
 };
 
 Recipes.renderCurrentRecipes = function(recipes) {
-  $('#try-recipes').toggleClass("hidden");
+  $('#try-recipes').removeClass("hidden");
+  $('.recipe-square').empty();
   for (var i = 0; i < recipes.length; i++) {
-    $("#recipe-" + i).empty();
     $("#recipe-" + i).append('<p>' + recipes[i].name + '</p>');
   }
 };
