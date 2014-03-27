3fe36ca5eae3ad7a6584d0d5b715029873f635d7
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-03-26 20:52:38 -0400

    added styling on app title and buttons

--- app/assets/javascripts/skills.js
+++ app/assets/javascripts/skills.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
 
 Skills.changeColor = function(event) {
   event.preventDefault();
+  $(this).toggleClass("active");
   $(this).toggleClass("btn-primary");
   $(this).toggleClass("btn-success");
+
   return false;
 };
